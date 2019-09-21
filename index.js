80ff79a7347b1a48349f5386659945eba50401ed
cdrishinski <cdrishinski@gmail.com>
2019-09-20 21:34:59 -0600

    adding styles, functionality compmlete

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
 function tipCalculation (){
-    alert("button working")
+    let bill = document.getElementById("bill").value;
+    let tip = document.getElementById("tip").value;
+    let partySize = document.getElementById("party-size").value;
+    let total;
+
+    // create error handling if bill is blank
+    if (bill == '') {
+        return alert("Please enter a Bill Amount")
+    }
+    // create calculation of tip size
+    total = (bill * tip )/ partySize;
+
+    // post answer to id="answered-section"
+
+    document.getElementById("phrase-section").innerHTML = "Tip between all parties is -" ;
+    document.getElementById("answered-section").innerHTML = "$" + total.toFixed(2);
+
 }
